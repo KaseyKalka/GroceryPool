7d6661864e899879d80f11c8c6ecedb4d7ea43b5
sethhatchel <shhatch@ilstu.edu>
2020-12-09 17:33:31 -0600

    Connected MessagesActivity to HomeScreen button

--- app/src/main/java/com/it326/grocerypool/ui/login/LoggedInUser.java
+++ app/src/main/java/com/it326/grocerypool/ui/login/LoggedInUser.java
@@ -2,9 +2,13 @@ package com.it326.grocerypool.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.it326.grocerypool.R;
+import com.it326.grocerypool.messaging.MessagingActivity;
 
 public class LoggedInUser extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class LoggedInUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in_user);
+
+        Button messagesButton = findViewById(R.id.Messages);
+        messagesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoggedInUser.this, MessagingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
