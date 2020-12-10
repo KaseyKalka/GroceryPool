23e2883051468640c793e5f125fcb562c9da1f67
JFlash30 <jasontsmith35@gmail.com>
2020-12-10 01:22:48 -0600

    EditProfile and View

---
+++ app/src/main/java/com/it326/grocerypool/profile/ViewRiderProfile.java
@@ -0,0 +1,32 @@
+package com.it326.grocerypool.profile;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.it326.grocerypool.R;
+
+public class ViewRiderProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other_user_profile);
+
+        TextView usernameEditText = findViewById(R.id.emailView);
+        usernameEditText.setText("TODO");//TODO
+
+        ImageView sendmess = findViewById(R.id.imageButton2);
+
+        sendmess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+    }
+}
